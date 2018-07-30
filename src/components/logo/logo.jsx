bbf7356f88b1657998f5a063bdb2602e795131a3
teachat <teachat@aliyun.com>
2018-07-30 17:02:33 +0800

    完成注册与登录静态页面

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import logo from './logo.png'
+import './logo.less'
+/**
+ *  简单的显示Logo的组件
+ */
+
+export default function Logo( ) {
+    return (
+        <div className="logo-container">
+            <img src={logo} alt="logo" className='logo-img' />
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {HashRouter, Route, Switch} from 'react-router-dom'
+import {Provider} from 'react-redux'
+
+import store from './redux/store'
 import Register from './containers/register/register'
 import Login from './containers/login/login'
 import Main from './containers/main/main'
 
 ReactDOM.render((
-    <HashRouter>
-        <Switch>
-            <Route path='/login' component={Login} />
-            <Route path='/register' component={Register} />
-            <Route component={Main} />
-        </Switch>
-    </HashRouter>
+    <Provider store={store}>
+        <HashRouter>
+            <Switch>
+                <Route path='/login' component={Login} />
+                <Route path='/register' component={Register} />
+                <Route component={Main} />      { /*默认路由*/}
+            </Switch>
+        </HashRouter>
+    </Provider>
+
 ) , document.getElementById('root'))
